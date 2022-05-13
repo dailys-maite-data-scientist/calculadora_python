8394c57dcda89b19819525c7003022839268de24
Maite <dailysmaite@gmail.com>
2022-05-13 10:05:24 -0300

    commit inicial

---
+++ python_calculator.py
@@ -0,0 +1,13 @@
+print ("********************Python Calculator***************************")
+entrada_opcion = int(input("Seleccione o número da opção desejada: 1. Soma, 2. resta, 3. multiplicação, 4. Divisão: "))
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+def calculadora (e, x, y):
+	if (e == 1):
+		print (x "+" y "=" x+y)
+	else ():
+		print ("aaa")
+
+calculadora (entrada_opcion, num1, num2)
