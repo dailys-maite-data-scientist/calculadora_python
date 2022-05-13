f7fe1eeb52aad52fb1fea68f93efb43c6db3a949
Maite <dailysmaite@gmail.com>
2022-05-13 11:36:48 -0300

    commit inicial

--- python_calculator.py
+++ python_calculator.py
@@ -1,13 +1,48 @@
-print ("********************Python Calculator***************************")
-entrada_opcion = int(input("Seleccione o número da opção desejada: 1. Soma, 2. resta, 3. multiplicação, 4. Divisão: "))
+print("\n**********************Python Calculator***************************")
 
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
+def soma (x, y):
+	return x + y
+		
+def resta (x, y):
+	return x - y
 
-def calculadora (e, x, y):
-	if (e == 1):
-		print (x "+" y "=" x+y)
-	else ():
-		print ("aaa")
+def multi (x, y):
+	return x * y
 
-calculadora (entrada_opcion, num1, num2)
+def divi (x, y):
+	return x / y
+
+print("\nSelecione o número da operação desejada: \n")
+
+print("1. Soma")
+print("2. Resta")
+print("3. Multiplicação")
+print("4. Divisão")
+
+opciom = input("\nDigite sua opção (1/2/3/4): ")
+
+num1 = int(input("\nDigite o primeiro número: "))
+num2 = int(input("\nDigite o segundo número: "))
+
+if opciom == "1":
+	print("\n")
+	print(num1, "+", num2, "=", soma(num1, num2))
+	print("\n")
+
+elif opciom == "2":
+	print("\n")
+	print(num1, "-", num2, "=", resta(num1, num2))
+	print("\n")
+
+elif opciom == "3":
+	print("\n")
+	print(num1, "*", num2, "=", multi(num1, num2))
+	print("\n")
+
+elif opciom == "4":
+	print("\n")
+	print(num1, "/", num2, "=", divi(num1, num2))
+	print("\n")
+
+else:
+	print("\nOpção Inválida!")
